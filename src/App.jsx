693915683af8752d2d09fa9da164159ed8945235
Samuel Onoja <samuelonoja0@gmail.com>
2025-02-14 22:36:04 -0800

    initial setup dependencies and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+import Homepage from "./pages/Homepage"
 
 function App() {
-
-
   return (
     <>
-    <h1 className='textgi-blue-500'>final Clean up</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element ={<Homepage/>}/>
+      </Routes>
+      <Footer/>
+
+      
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+       <div className="flex justify-around sticky top-0 h-25 py-1 bg-purple-100 gap-2">
+        </div>
+           
+    </div>
+  )
+}
+
+export default Navbar

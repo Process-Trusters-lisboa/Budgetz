e4c448814671b16e81db69c49594010a89d0f342
Samuel Onoja <samuelonoja0@gmail.com>
2025-02-20 22:48:52 -0800

    modify homepage

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import expensehero from '../assets/expensehero.jpg';
+import { useState, useEffect } from 'react';
 
 function Hero() {
+
+  const [headingColor, setHeadingColor] = useState("text-gray-700");
+
+
+  const colorOptions = [
+    "text-gray-700",
+    "text-blue-400",
+    "text-cyan-600",
+    "text-green-400",
+    "text-yellow-500",
+    "text-purple-500"
+    
+  ];
+
+   useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * colorOptions.length);
+      setHeadingColor(colorOptions[randomIndex]);
+    }, 3000); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
+
   return (
     <div className="max-h-screen mx-auto">
       <section>
         <div className="flex flex-col-reverse mx-auto p-6 md:flex-row items-center">
           <div className="flex flex-col space-y-10">
-            <h1 className="mt-22 text-5xl text-center font-semibold text-gray-700">
-              Your Smart Budget Manager
-            </h1>
+            <h1 className={`mt-22 text-5xl text-center font-semibold ${headingColor} animate-slide transition-colors duration-1000`}>
+        Your Smart Budget Manager
+      </h1>
             <p className="max-w-md mx-auto text-3xl text-center text-gray-600">
               The easy way to take control of your finances. Whether you're
               saving for a goal or managing daily expenses, our platform helps
